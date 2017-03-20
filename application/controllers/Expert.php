e029bad8acd871778a5f3e663d443974d25e07e6
Wiratama Paramasatya <wiratamaparamasatya@gmail.com>
2017-03-20 23:07:03 +0700

    Add Expert Subsystem

---
+++ application/controllers/Expert.php
@@ -0,0 +1,81 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Expert extends CI_Controller {
+
+	public $GLOBAL_DATA_SUBSYSTEM_VALUE;
+	private $GLOBAL_PARAM_UUID_SUBSYSTEM_EXPERT = "4249cbf4-06ba-11e7-88db-c454448293a1";
+
+	public function __construct() {
+		parent::__construct();
+
+		$this->GLOBAL_DATA_SUBSYSTEM_VALUE = $this->expert_model->get_one('subsystem_value', 'ms_subsystem', $this->session->userdata('uuid_ms_subsystem'));
+
+		if(empty($this->session->userdata('is_logged_in')) && $this->session->userdata('uuid_ms_subsystem') != $this->GLOBAL_PARAM_UUID_SUBSYSTEM_EXPERT) {
+			$this->session->set_flashdata('msg','<div class="alert alert-danger text-center">You dont have an access to view this page!</div>');
+			redirect('');
+		}
+	}
+
+	public function index() {
+
+	}
+
+	public function home() {
+		$this->load->view('expert/home');
+	}
+
+  public function list_data_training() {
+		$data['data_training'] = $this->data_model->get_all_data_training();
+
+		$a=0;
+		$bi_percentage[$a] = 0;
+		foreach ($data['data_training'] as $row) {
+			$bi_percentage[$a] = $row->bi_rate * 100;
+			$a++;
+		}
+
+		$this->load->view('expert/list_data_training', array('data_training' => $data['data_training'],
+																												'bi_percentage' => $bi_percentage));
+	}
+
+	public function list_data_uji() {
+		$data['data_uji'] = $this->data_model->get_all_data_uji();
+
+		$a=0;
+		$bi_percentage[$a] = 0;
+		foreach ($data['data_uji'] as $row) {
+			$bi_percentage[$a] = $row->bi_rate * 100;
+			$a++;
+		}
+
+		$this->load->view('expert/list_data_uji', array('data_uji' => $data['data_uji'],
+																									 'bi_percentage' => $bi_percentage));
+	}
+
+	public function pre_forecasting() {
+		$data['data_training'] = $this->data_model->get_all_data_training();
+		$data['data_uji'] = $this->data_model->get_all_data_uji();
+
+		$a=0;
+		$bi_percentage_training[$a] = 0;
+		foreach ($data['data_training'] as $row) {
+			$bi_percentage_training[$a] = $row->bi_rate * 100;
+			$a++;
+		}
+
+		$b=0;
+		$bi_percentage_uji[$b] = 0;
+		foreach ($data['data_uji'] as $row) {
+			$bi_percentage_uji[$b] = $row->bi_rate * 100;
+			$b++;
+		}
+
+		$this->load->view('expert/pre_forecasting', array('data_training' => $data['data_training'],
+																											'data_uji' => $data['data_uji'],
+																											'bi_percentage_training' => $bi_percentage_training,
+																											'bi_percentage_uji' => $bi_percentage_uji));
+	}
+
+}
---
+++ application/views/expert/pre_forecasting.php
@@ -0,0 +1,344 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>BI Rate Forecasting | Data Training</title>
+  <!-- Tell the browser to be responsive to screen width -->
+  <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+  <!-- Bootstrap 3.3.6 -->
+  <link rel="stylesheet" href="<?php echo base_url('assets/'); ?>bootstrap/css/bootstrap.min.css">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css">
+  <!-- Ionicons -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="<?php echo base_url('assets/'); ?>dist/css/AdminLTE.min.css">
+  <!-- AdminLTE Skins. Choose a skin from the css/skins
+       folder instead of downloading all of them to reduce the load. -->
+  <link rel="stylesheet" href="<?php echo base_url('assets/'); ?>dist/css/skins/_all-skins.min.css">
+  <!-- iCheck -->
+  <link rel="stylesheet" href="<?php echo base_url('assets/'); ?>plugins/iCheck/flat/blue.css">
+  <!-- Morris chart -->
+  <link rel="stylesheet" href="<?php echo base_url('assets/'); ?>plugins/morris/morris.css">
+  <!-- jvectormap -->
+  <link rel="stylesheet" href="<?php echo base_url('assets/'); ?>plugins/jvectormap/jquery-jvectormap-1.2.2.css">
+  <!-- Date Picker -->
+  <link rel="stylesheet" href="<?php echo base_url('assets/'); ?>plugins/datepicker/datepicker3.css">
+  <!-- Daterange picker -->
+  <link rel="stylesheet" href="<?php echo base_url('assets/'); ?>plugins/daterangepicker/daterangepicker.css">
+  <!-- bootstrap wysihtml5 - text editor -->
+  <link rel="stylesheet" href="<?php echo base_url('assets/'); ?>plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css">
+
+  <link rel="stylesheet" href="<?php echo base_url('assets/'); ?>plugins/datatables/dataTables.bootstrap.css">
+
+  <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+  <!--[if lt IE 9]>
+  <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+  <![endif]-->
+</head>
+<body class="hold-transition skin-blue sidebar-mini fixed">
+<div class="wrapper">
+
+  <header class="main-header">
+    <!-- Logo -->
+    <a href="<?php echo base_url('expert/home')?>" class="logo">
+      <!-- mini logo for sidebar mini 50x50 pixels -->
+      <span class="logo-mini"><b>BI</b>F</span>
+      <!-- logo for regular state and mobile devices -->
+      <span class="logo-lg"><b>BI Rate</b>Forecasting</span>
+    </a>
+    <!-- Header Navbar: style can be found in header.less -->
+    <nav class="navbar navbar-static-top">
+      <!-- Sidebar toggle button-->
+      <a href="#" class="sidebar-toggle" data-toggle="offcanvas" role="button">
+        <span class="sr-only">Toggle navigation</span>
+      </a>
+
+      <div class="navbar-custom-menu">
+        <ul class="nav navbar-nav">
+          <li class="dropdown user user-menu">
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+              <span class="hidden-xs"><?php echo $this->session->userdata('full_name'); ?></span>
+            </a>
+            <ul class="dropdown-menu">
+              <!-- User image -->
+              <li class="user-header">
+                <!-- <img src="dist/img/user2-160x160.jpg" class="img-circle" alt="User Image"> -->
+                <p>
+                  <?php echo $this->session->userdata('full_name'); ?>
+                  <small><?php echo $this->GLOBAL_DATA_SUBSYSTEM_VALUE; ?></small>
+                  <small>Member since <?php echo $this->session->userdata('dtm_crt'); ?></small>
+                </p>
+              </li>
+              <!-- Menu Footer-->
+              <li class="user-footer">
+                <div class="pull-left">
+                  <a href="#" class="btn btn-default btn-flat">Profile</a>
+                </div>
+                <div class="pull-right">
+                  <a href="<?php echo base_url('auth/logout'); ?>" class="btn btn-default btn-flat">Sign out</a>
+                </div>
+              </li>
+            </ul>
+          </li>
+          <!-- Control Sidebar Toggle Button -->
+          <li>
+            <a href="#" data-toggle="control-sidebar"><i class="fa fa-gears"></i></a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  </header>
+  <!-- Left side column. contains the logo and sidebar -->
+  <aside class="main-sidebar">
+    <!-- sidebar: style can be found in sidebar.less -->
+    <section class="sidebar">
+      <!-- /.search form -->
+      <!-- sidebar menu: : style can be found in sidebar.less -->
+      <ul class="sidebar-menu">
+        <li class="header">MAIN NAVIGATION</li>
+        
+        <li class="treeview">
+          <a href="#">
+            <i class="fa fa-database"></i> <span>Master Data</span>
+            <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+          </a>
+          <ul class="treeview-menu">
+            <li>
+              <a href="#"><i class="fa fa-folder-open"></i> Data Latih
+                <span class="pull-right-container">
+                  <i class="fa fa-angle-left pull-right"></i>
+                </span>
+              </a>
+              <ul class="treeview-menu">
+                <li><a href="#"><i class="fa fa-plus"></i> Tambah Data Latih</a></li>
+                <li><a href="<?php echo base_url('expert/list-data-training'); ?>"><i class="fa fa-list-ul"></i> List Data Latih</a></li>
+              </ul>
+            </li>
+            <li>
+              <a href="#"><i class="fa fa-folder"></i> Data Uji
+                <span class="pull-right-container">
+                  <i class="fa fa-angle-left pull-right"></i>
+                </span>
+              </a>
+              <ul class="treeview-menu">
+                <li><a href="#"><i class="fa fa-plus"></i> Tambah Data Uji</a></li>
+                <li><a href="<?php echo base_url('expert/list-data-uji'); ?>"><i class="fa fa-list-ul"></i> List Data Uji</a></li>
+              </ul>
+            </li>
+          </ul>
+        </li>
+
+        <li class="treeview active">
+          <a href="#">
+            <i class="fa fa-fast-forward"></i> <span>Peramalan</span>
+          </a>
+        </li>
+    </section>
+    <!-- /.sidebar -->
+  </aside>
+
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <h1>
+        Dashboard
+        <small>Pra Peramalan</small>
+      </h1>
+      <ol class="breadcrumb">
+        <li><a href="<?php echo base_url('client/home')?>"><i class="fa fa-dashboard"></i> Home</a></li>
+        <li class="fa fa-users active"> Pra Peramalan</li>
+      </ol>
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+      <div class="box">
+        <div class="box-header">
+          <?php echo $this->session->flashdata('msg'); ?>
+          <h3 class="box-title">List Data Latih</h3>
+        </div>
+        <!-- /.box-header -->
+        <div class="box-body">
+          <table id="listdatatraining" class="table table-bordered table-striped">
+            <thead>
+            <tr>
+              <th>Bulan</th>
+              <th>Tahun</th>
+              <th>BI rate</th>
+            </tr>
+            </thead>
+            <tbody>
+              <?php
+                $a=0;
+                foreach($data_training as $row)
+                {
+                  echo '<tr>';
+                  echo '<td>'.$row->bulan.'</td>';
+                  echo '<td>'.$row->tahun.'</td>';
+                  echo '<td>'.$bi_percentage_training[$a].' %</td>';
+                  echo '</tr>';
+                  $a++;
+                }
+              ?>
+            </tbody>
+            <tfoot>
+              <tr>
+                <th>Bulan</th>
+                <th>Tahun</th>
+                <th>BI rate</th>
+              </tr>
+            </tfoot>
+          </table>
+        </div>
+        <!-- /.box-body -->
+      </div>
+      <!-- /.box -->
+
+      <div class="box">
+        <div class="box-header">
+          <?php echo $this->session->flashdata('msg'); ?>
+          <h3 class="box-title">List Data Uji</h3>
+        </div>
+        <!-- /.box-header -->
+        <div class="box-body">
+          <table id="listdatauji" class="table table-bordered table-striped">
+            <thead>
+            <tr>
+              <th>Bulan</th>
+              <th>Tahun</th>
+              <th>BI rate</th>
+            </tr>
+            </thead>
+            <tbody>
+              <?php
+                $b=0;
+                foreach($data_uji as $row)
+                {
+                  echo '<tr>';
+                  echo '<td>'.$row->bulan.'</td>';
+                  echo '<td>'.$row->tahun.'</td>';
+                  echo '<td>'.$bi_percentage_uji[$b].' %</td>';
+                  echo '</tr>';
+                  $b++;
+                }
+              ?>
+            </tbody>
+            <tfoot>
+              <tr>
+                <th>Bulan</th>
+                <th>Tahun</th>
+                <th>BI rate</th>
+              </tr>
+            </tfoot>
+          </table>
+        </div>
+        <!-- /.box-body -->
+      </div>
+      <!-- /.box -->
+
+      <button type="button" class="btn btn-block btn-primary btn-flat">Peramalan dan Pengujian</button>
+    </section>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
+  <footer class="main-footer">
+    <div class="pull-right hidden-xs">
+      <b>Version</b> 1.0.0
+    </div>
+    <strong>Copyright &copy; 2017 <a href="https://www.linkedin.com/in/wiratama-paramasatya-40b387133/">Wiratama Paramasatya</a>.</strong> All rights
+    reserved.
+  </footer>
+
+  <!-- Control Sidebar -->
+  <aside class="control-sidebar control-sidebar-dark">
+    <!-- Create the tabs -->
+    <ul class="nav nav-tabs nav-justified control-sidebar-tabs">
+      <li><a href="#control-sidebar-home-tab" data-toggle="tab"><i class="fa fa-home"></i></a></li>
+      <li><a href="#control-sidebar-settings-tab" data-toggle="tab"><i class="fa fa-gears"></i></a></li>
+    </ul>
+    <!-- Tab panes -->
+    <div class="tab-content">
+      <!-- Home tab content -->
+      <div class="tab-pane" id="control-sidebar-home-tab">
+        <h3 class="control-sidebar-heading">Recent Activity</h3>
+        <ul class="control-sidebar-menu">
+        </ul>
+        <!-- /.control-sidebar-menu -->
+
+        <h3 class="control-sidebar-heading">Tasks Progress</h3>
+        <ul class="control-sidebar-menu">
+
+        </ul>
+        <!-- /.control-sidebar-menu -->
+
+      </div>
+      <!-- /.tab-pane -->
+      <!-- Stats tab content -->
+      <div class="tab-pane" id="control-sidebar-stats-tab">Stats Tab Content</div>
+      <!-- /.tab-pane -->
+      <!-- Settings tab content -->
+      <div class="tab-pane" id="control-sidebar-settings-tab">
+        <form method="post">
+          <h3 class="control-sidebar-heading">General Settings</h3>
+      </div>
+      <!-- /.tab-pane -->
+    </div>
+  </aside>
+  <!-- /.control-sidebar -->
+  <!-- Add the sidebar's background. This div must be placed
+       immediately after the control sidebar -->
+  <div class="control-sidebar-bg"></div>
+</div>
+<!-- ./wrapper -->
+
+<!-- jQuery 2.2.3 -->
+<script src="<?php echo base_url('assets/'); ?>plugins/jQuery/jquery-2.2.3.min.js"></script>
+<!-- jQuery UI 1.11.4 -->
+<script src="https://code.jquery.com/ui/1.11.4/jquery-ui.min.js"></script>
+<!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
+<script>
+  $.widget.bridge('uibutton', $.ui.button);
+</script>
+<!-- Bootstrap 3.3.6 -->
+<script src="<?php echo base_url('assets/'); ?>bootstrap/js/bootstrap.min.js"></script>
+<!-- Sparkline -->
+<script src="<?php echo base_url('assets/'); ?>plugins/sparkline/jquery.sparkline.min.js"></script>
+<!-- jvectormap -->
+<script src="<?php echo base_url('assets/'); ?>plugins/jvectormap/jquery-jvectormap-1.2.2.min.js"></script>
+<script src="<?php echo base_url('assets/'); ?>plugins/jvectormap/jquery-jvectormap-world-mill-en.js"></script>
+<!-- jQuery Knob Chart -->
+<script src="<?php echo base_url('assets/'); ?>plugins/knob/jquery.knob.js"></script>
+<!-- daterangepicker -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.11.2/moment.min.js"></script>
+<script src="<?php echo base_url('assets/'); ?>plugins/daterangepicker/daterangepicker.js"></script>
+<!-- datepicker -->
+<script src="<?php echo base_url('assets/'); ?>plugins/datepicker/bootstrap-datepicker.js"></script>
+<!-- Bootstrap WYSIHTML5 -->
+<script src="<?php echo base_url('assets/'); ?>plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js"></script>
+<!-- Slimscroll -->
+<script src="<?php echo base_url('assets/'); ?>plugins/slimScroll/jquery.slimscroll.min.js"></script>
+<!-- FastClick -->
+<script src="<?php echo base_url('assets/'); ?>plugins/fastclick/fastclick.js"></script>
+<!-- AdminLTE App -->
+<script src="<?php echo base_url('assets/'); ?>dist/js/app.min.js"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="<?php echo base_url('assets/'); ?>dist/js/demo.js"></script>
+<!-- DataTables -->
+<script src="<?php echo base_url('assets/'); ?>plugins/datatables/jquery.dataTables.min.js"></script>
+<script src="<?php echo base_url('assets/'); ?>plugins/datatables/dataTables.bootstrap.min.js"></script>
+<!-- page script -->
+<script>
+  $(function () {
+    $("#listdatatraining").DataTable();
+    $("#listdatauji").DataTable();
+  });
+</script>
+</body>
+</html>
